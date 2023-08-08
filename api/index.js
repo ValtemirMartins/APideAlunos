c13b89f262fc6ed5f3352897a53995021111f9af
valtemir martins <valmartinsfilho@gmail.com>
2023-08-08 16:33:10 -0300

    INTEGRAÇÃO DE DADOS E

--- api/index.js
+++ api/index.js
@@ -1,22 +1,40 @@
 const { ApolloServer} = require('apollo-server')
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const path = require('path')
+
+
 const userSchema = require('./user/schema/user.graphql')
 const userResolvers = require('./user/resolvers/userResolvers')
 const UsersAPI = require('./user/datasource/user')
 
-const typeDefs = [userSchema]
-const resolvers = [userResolvers]
+const turmaSchema = require('./turma/schema/turma.graphql');
+const turmaResolvers = require('./turma/resolvers/turmaRsolvers');
+const TurmasAPI = require('./turma/datasource/turma')
+
+const typeDefs = [userSchema, turmaSchema];
+const resolvers = [userResolvers, turmaResolvers];
+
+const dbConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+  filename: path.resolve( __dirname, './data/database.db')
+  }
+
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const server = new ApolloServer({
-  typeDefs, 
-  resolvers,
+  schema,
   dataSources: () => {
     return {
-      usersAPI: new UsersAPI()
+      usersAPI: new UsersAPI(),
+      turmasAPI: new TurmasAPI(dbConfig)
+    };
+  },
+});
 
-    }
-  }
-})
-
-server.listen().then(({url}) =>{
-  console.log(`Servidor rodando na porta ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`Servidor rodando na porta ${url}`);
+});
